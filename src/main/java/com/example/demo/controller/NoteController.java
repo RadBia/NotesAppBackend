b8ece504c2ed6d93e971240a41fc2680b20dbdc9
RadBia <rad.bialkowski@gmail.com>
2023-03-25 12:43:24 +0100

    Add deleteNote method to NoteController for deleting a note by id

--- src/main/java/com/example/demo/controller/NoteController.java
+++ src/main/java/com/example/demo/controller/NoteController.java
@@ -8,6 +8,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.LinkedHashMap;
+
 
 // @CrossOrigin(origins = "http://localhost:3000")
 @RestController
@@ -19,9 +21,18 @@ public class NoteController {
     @Autowired
     private UserRepository userRepository;
 
+
     @GetMapping("/{noteId}")
     public ResponseEntity<Note> getNote(@PathVariable(value = "noteId") Long noteId) {
         Note note = noteRepository.findById(noteId).orElseThrow();
         return new ResponseEntity<>(note, HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{noteId}")
+    public ResponseEntity<?> deleteNote(@PathVariable("noteId") long id) {
+        LinkedHashMap<String, String> map_res = new LinkedHashMap<>();
+        map_res.put("msg", "Usunięto"); // deleted msg
+        noteRepository.deleteById(id);
+        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
     }
 }
